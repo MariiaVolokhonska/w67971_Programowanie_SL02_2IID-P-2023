fc41cdea1a3798238ff98be777c5033032f29f20
w67971 <mvolokhonskaya@gmail.com>
2023-04-17 01:58:42 +0300

    1

--- Lab4/2.js
+++ Lab4/2.js
@@ -25,7 +25,7 @@ const student={
     }
 };
 student.Sred()
-/*Task 3*/
+/*Task 3 and 4*/
 class Car{
     constructor(mark,model,year,color,speed){
         this.mark=mark;
@@ -36,10 +36,61 @@ class Car{
     }
     Sp(){
         const c=30;
-        f=this.speed+c;
-        s=this.speed-c;
+        var k=this.speed
+        var f=k+c;
+        var s=k-c;
         console.log("after icreasing: "+f+" after decreasing: "+s)
+    };
+    Inf(){
+        console.log(this.mark+" "+this.model+"("+this.year+"), Kolor:"+this.color+",Speed:"+this.speed)
+    };
+    Age(){
+        let date=new Date().getFullYear();
+         console.log("The age of the car is: ", date-this.year)
+     };
+
+}
+const honda=new Car("honda","CV",1998,"red",200);
+honda.Sp();
+honda.Inf();
+honda.Age();
+/*Task 5 and 6*/ 
+class Rectangle{
+    constructor(length,width){
+        this.length=length;
+        this.width=width;
+    }
+    Pole(){
+        console.log("Pole: ",this.width*this.length)
+    };
+    Obwod(){
+        console.log("Obwod: ",2*(this.width+this.length))
+    };
+    Check(){
+        if(this.width==this.length){
+            console.log("This rectangle is a square");
+        }
+        else{
+            console.log("This rectangle isn't square")
+        }
     }
 }
-const honda=new Car("honda","something",1998,"red",200)
-honda.Sp()
+rec1=new Rectangle(12,13);
+rec2=new Rectangle(4,4)
+rec1.Pole();
+rec1.Obwod();
+rec1.Check();
+rec2.Pole();
+rec2.Obwod();
+rec2.Check();
+function Creat(ln,wd,Rectangle){
+   const inc=new Rectangle(ln,wd);
+   document.write("Length is: ",inc.length,"  ");
+   document.write("Width is: ",inc.width)
+   inc.Pole();
+   inc.Obwod();
+   inc.Check();
+}
+var l=document.getElementById("pole");
+var w=document.getElementById("pole1");
+Creat(l,w,Rectangle);
