272d6661256eb7f5adc9bb232cf4b1dd313a9717
w67971 <mvolokhonskaya@gmail.com>
2023-05-12 14:45:23 +0300

    1

---
+++ Lab7_8/7_8.js
@@ -0,0 +1,43 @@
+/*const pole= document.getElementById("name");
+pole.addEventListener("input",()=>{
+    var v=pole.value;
+    v1=v.trim();
+    console.log(v1)
+    if (v==""){
+        alert("The pole is empty!");
+    }
+    if 
+
+
+    }
+);*/
+
+
+function validateForm() {
+    let x = document.forms["form"]["name"].value;
+    if (x == "") {
+        alert("This field must be filled out");
+        return false;
+    }
+}
+function validateForm2() {
+    let y = document.forms["form"]["nazwisko"].value;
+    if (y == "") {
+        alert("This field must be filled out");
+        return false;
+    }
+}
+const z=document.getElementById("surn");
+z.addEventListener("input",()=>{
+    let k=z.value;
+    let k1=k.length;
+    console.log("It has "+k1+" signs");  
+});
+function validateEmail(){
+    const e=document.forms["form"]["email"];
+    var e1=e.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(e1)) {
+        alert("Proszę podać poprawny adres e-mail.");
+ }
+};
